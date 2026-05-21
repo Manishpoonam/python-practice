c8600459808671ad6be3cbd16211f5475d1530d2
Manishpoonam <manishkumarkashyap14@gmail.com>
2026-05-21 23:17:04 +0530

    updated 3rd progrma

--- 3rd.py
+++ 3rd.py
@@ -3,14 +3,14 @@
 
 #-------------loop----
 
-# for x in range(6):
-#     print(x)
+for x in range(6):
+    print(x)
 
-# for x in range(2, 6):
-#     print(x)
+for x in range(2, 6):
+    print(x)
 
-# for x in range (1,20,2):
-#     print(x)
+for x in range (1,20,2):
+    print(x)
 
 
 
